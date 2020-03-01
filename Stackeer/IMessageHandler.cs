6f92966470695f44fba57544748a6a007ce54d29
Jacob Millward <jacob@millward.dev>
2020-03-01 16:18:46 +0000

    Make `IMessageHandler` internal

---
+++ Stackeer/IMessageHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+
+namespace Stackeer
+{
+    internal interface IMessageHandler
+    {
+        ImmutableArray<IMessage> Process(IMessage input);
+    }
+}
--- Stackeer/MessageHandler.cs
+++ Stackeer/MessageHandler.cs
@@ -2,11 +2,6 @@
 
 namespace Stackeer
 {
-    public interface IMessageHandler
-    {
-        ImmutableArray<IMessage> Process(IMessage input);
-    }
-
     public abstract class MessageHandler<T> : IMessageHandler where T : IMessage
     {
         public ImmutableArray<IMessage> Process(IMessage input) => Process((T)input);
